8c8a855dd60759fadcb8e55b817a3e45e3e7c37e
Supreme Developer <unconfigured@null.spigotmc.org>
2019-01-12 11:32:51 -0200

    Gerador de Lixo

---
+++ Gerador de Lixo/Gerar Lixo/Form1.Designer.cs
@@ -0,0 +1,121 @@
+﻿namespace Gerar_Lixo
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 38);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(219, 42);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Gerar";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Increment = new decimal(new int[] {
+            1000,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Location = new System.Drawing.Point(12, 12);
+            this.numericUpDown1.Maximum = new decimal(new int[] {
+            -1304428545,
+            434162106,
+            542,
+            0});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(108, 20);
+            this.numericUpDown1.TabIndex = 1;
+            this.numericUpDown1.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(12, 86);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(219, 23);
+            this.progressBar1.TabIndex = 2;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "b",
+            "Kb",
+            "Mb",
+            "Gb",
+            "Tb"});
+            this.comboBox1.Location = new System.Drawing.Point(126, 11);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(105, 21);
+            this.comboBox1.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(247, 121);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Gerador de Lixo";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.ComboBox comboBox1;
+    }
+}
+
---
+++ Gerador de Lixo/Gerar Lixo/Form1.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Threading;
+
+namespace Gerar_Lixo
+{
+    public partial class Form1 : Form
+    {
+        //Desenvolvido por Supreme Developer (ConradoPSA)
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog SFD = new SaveFileDialog();
+
+            if (SFD.ShowDialog() == DialogResult.OK)
+            {
+                int qtd = Convert.ToInt32(numericUpDown1.Value);                
+                int cbIndex = comboBox1.SelectedIndex;
+                FileStream FS = File.OpenWrite(SFD.FileName);
+
+                /*Invoca a Função Escrever em uma nova Thread
+                 para que o form principal não fique inativo durante
+                 o processo*/
+                Thread th = new Thread(() => Gerar(FS, qtd, cbIndex));
+                th.Start();
+
+            }
+        }
+
+        const string msgboxTitulo = "Gerador de Lixo";
+        private void Gerar(FileStream FS, int val, int index)
+        {
+            progressBar1.Value = 0;
+
+            //index: b = 0, kb = 1, mb = 2, gb = 3, tb = 4
+            //qtd = val  * 1024 ^ index
+            Int64 qtd = val * (Int64)Math.Pow(1024.0, index);
+
+            #region Curiosidade
+            //1 Terabyte = 1.099.511.627.776 bytes
+            //Valor máximo do Int64 = 9.223.372.036.854.775.807
+            //Portanto, limite de geração de lixo ~= 8.388.607,99 Terabytes
+            #endregion
+
+            try
+            {
+                for (Int64 i = 1; i <= qtd; i++)
+                {
+                    FS.WriteByte((byte)0);
+
+                    //Atualiza a cada 104857600 repetições (100 Mb) ou na última repetição
+                    if (i % 104857600 == 0 || i == qtd)
+                    {
+                        FS.Flush();
+
+                        //Invoca a thread principal e executa ações
+                        progressBar1.Invoke(new Action(() => {
+                            progressBar1.Value = (int)((i * 1.0f / qtd) * 100);
+
+                            if (progressBar1.Value == 100)
+                            {
+                                MessageBox.Show("Lixo Gerado!", msgboxTitulo,
+                                    MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+                                progressBar1.Value = 0;
+                            }
+
+                        }));
+
+                    }
+                }
+
+                FS.Dispose();
+            }
+            catch (IOException)
+            {
+                //Exceção gerada ao não ter mais espaço disponível
+
+                progressBar1.Invoke(new Action(() => {
+                    progressBar1.Value = 100;
+
+                    MessageBox.Show("Espaço completamente preenchido!", msgboxTitulo,
+                        MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+                    MessageBox.Show("Lixo Gerado!", msgboxTitulo,
+                        MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+                    progressBar1.Value = 0;
+                                       
+                }));
+            }
+            
+
+            
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //Padrão
+            comboBox1.Text = "Kb";
+        }
+    }
+}
